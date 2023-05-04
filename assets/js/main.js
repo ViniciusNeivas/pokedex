a903a1b9c4715cb1a59769f13e8c02a6c2e98292
ViniciusNeivas <viniciusneivas@outlook.com.br>
2023-05-04 15:49:12 -0300

    Adicão do arquivo poke-api.js onde é feita o contato com a api via fetch, onde é ontida a response que é transformada em json e onde obtem o result. No arquivo main.js foi feita a busca da lista de pokemon e transformada em uma lista HTML, concatenando as lista obtidas com o join()

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,27 @@
-console.log("Sucesso");
+function convertPokemonToLi(pokemon) {
+  return `
+    <li class="pokemon">
+                <span class="number-pokedex">#001</span>
+                <span class="name">${pokemon.name}</span> 
+
+                <section class="detail">
+                    <ol class="types">
+                        <li class="type">
+                            Grass
+                        </li>
+                        <li class="type">
+                            Poison
+                        </li>
+                    </ol>
+
+                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+                </section>
+            </li>
+    `;
+}
+
+const pokemonList = document.getElementById("pokemon-list");
+pokeApi.getPokemons().then((pokemons = []) => {
+  pokemonList.innerHTML = pokemons.map(convertPokemonToLi).join("");
+ 
+});
